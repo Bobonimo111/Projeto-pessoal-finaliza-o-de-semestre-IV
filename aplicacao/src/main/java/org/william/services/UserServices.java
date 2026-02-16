d45ffbf4922b661899633216b1543cbb138e0bde
Thinkpad_will <carlosmaycon443@gmail.com>
2026-02-15 21:47:41 -0300

    adicionando controle de rotas restful

--- aplicacao/src/main/java/org/william/services/UserServices.java
+++ aplicacao/src/main/java/org/william/services/UserServices.java
@@ -27,20 +27,31 @@ public class UserServices {
     @Transactional
     public void createUser(PostUserDTO postUserDTO) {
         UserEntity userEntity = this.userMapper.toEntity(postUserDTO);
-        this.userRepository.saveUser(userEntity);
+        this.userRepository.persist(userEntity);
     }
 
     @Transactional
     public List<GetUserDTO> getUsers() {
         return this.userRepository.listAll()
         .stream()
-        .map(m -> this.userMapper.toGetUserDto(m))
+        .map(this.userMapper::toGetUserDto)
         .toList();
     }
 
-    public Object getUserById(int id) {
+    @Transactional
+    public UserEntity getUserById(int id) {
+        return this.userRepository.findById(id);
     }
 
     public void updateFullUser(PostUserDTO postUserDTO, int id) {
+        UserEntity recivedUserEntity = this.userMapper.toEntity(postUserDTO);
+
+        UserEntity dbUserEntity = this.userRepository.findById(id);
+
+        dbUserEntity.setName(recivedUserEntity.getName());
+        dbUserEntity.setLastName(recivedUserEntity.getLastName());
+
+        this.userRepository.persist(dbUserEntity);
+
     }
 }
